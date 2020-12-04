1a6f4a26abb980abb83cbe77817a55bab705709c
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-12-03 17:51:02 -0800

    finish pseudo-code

--- test_2.py
+++ test_2.py
@@ -9,3 +9,17 @@ The first two functions will display the number of grades and the average grade.
 There will be a function that will be used in order to display the percent of grades above
 the average grade of 54.17%. 
 """
+
+"""
+# Number of grades
+    return: 24
+
+# Average grade
+    return: 83.25
+
+# main 
+    calculate_percent_above_average
+    return 54.17%
+    integers = (78,67,56,99,80,83,82,91,94,95,77,88,85,92,91,79,88,82,81,86,94,93,92,45)
+    
+"""
